e97d98f69ef4eadcf114d77a20f875ed0b47aafd
Gabriel Leon <gabriel_leon23@hotmail.com>
2025-06-23 16:48:34 -0400

    criação do projeto e packages

---
+++ src/main/java/br/com/rpganuncios/RpganunciosApplication.java
@@ -0,0 +1,13 @@
+package br.com.rpganuncios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpganunciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpganunciosApplication.class, args);
+	}
+
+}
